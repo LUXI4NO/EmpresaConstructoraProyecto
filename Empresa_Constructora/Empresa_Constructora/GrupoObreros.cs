e1cc53d1d33c5e39c4fed0d52333383077c7fa5c
Luciano Alvarez <140111840+LUXI4NO@users.noreply.github.com>
2025-06-03 11:36:54 -0300

    GrupoObreros.cs

--- Empresa_Constructora/Empresa_Constructora/GrupoObreros.cs
+++ Empresa_Constructora/Empresa_Constructora/GrupoObreros.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+using System;
+using System.Collections;
 
 namespace Empresa_Constructora
 {
@@ -6,13 +7,43 @@ namespace Empresa_Constructora
     {
         public int GrupoId { get; set; }
         public string NombreGrupo { get; set; }
-        public ArrayList ObrerosDelGrupo { get; set; }
+        public ArrayList ListaObreros { get; set; }
 
         public GrupoObreros(int id, string nombre)
         {
             GrupoId = id;
             NombreGrupo = nombre;
-            ObrerosDelGrupo = new ArrayList();
+            ListaObreros = new ArrayList();
+        }
+
+        public void AgregarObrero(Obrero obrero)
+        {
+            ListaObreros.Add(obrero);
+            Console.WriteLine("Se asignó a " + obrero.Nombre + " " + obrero.Apellido + " al grupo " + NombreGrupo);
+        }
+
+        public void EliminarObreros(int legajoObrero)
+        {
+            Obrero obreroARemover = null;
+
+            foreach (Obrero o in ListaObreros)
+            {
+                if (o.Legajo == legajoObrero)
+                {
+                    obreroARemover = o;
+                    break;
+                }
+            }
+
+            if (obreroARemover != null)
+            {
+                ListaObreros.Remove(obreroARemover);
+                Console.WriteLine("Obrero " + obreroARemover.Nombre + " " + obreroARemover.Apellido + " (Legajo: " + legajoObrero + ") eliminado del Grupo " + GrupoId);
+            }
+            else
+            {
+                Console.WriteLine("Obrero con legajo " + legajoObrero + " no encontrado en el Grupo " + GrupoId);
+            }
         }
     }
 }
